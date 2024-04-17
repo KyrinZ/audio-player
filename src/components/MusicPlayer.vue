6a6cd31d566de97b2638e91887998f2f4468a24d
Bayajit Mohammed <bayajitmohammed@gmail.com>
2024-04-17 14:56:50 +0600

    update the player controls

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,119 +1,135 @@
 <template>
-    <div class="text-white p-8 py-10 rounded-[3.5rem] w-full max-w-[500px] relative overflow-hidden"
-        style="background-image: url('https://source.unsplash.com/1600x900/?nature,water');">
-        <div class="absolute" style="backdrop-filter: blur(15px);background-color: #000000c7; inset: 0;"></div>
+    <div class="text-[#FAE9CF] p-8 py-10 rounded-[3.5rem] w-full max-w-[450px] relative overflow-hidden z-0 shadow-lg">
 
-        <div class="relative z-10">
-            <!-- Top Nav Header -->
-            <div class="flex justify-between items-center mb-4">
-                <button>
-                    <BackIcon class="w-8" />
-                </button>
-                <p>Now Playing</p>
-                <button>
-                    <BackIcon class="w-8 rotate-180" />
-                </button>
-            </div>
+        <img class="absolute w-full h-full object-cover z-[-2]" src="https://source.unsplash.com/1600x900/?nature,water"
+            alt="">
+        <div class="absolute inset-0 z-[-1]" style="backdrop-filter: blur(15px);background-color: #000000ad; inset: 0;">
+        </div>
 
-            <audio ref="audio" src="sample.music.mp3" @timeupdate="upateTime"></audio>
+        <!-- Top Nav Header -->
+        <div class="flex justify-between items-center mb-4">
+            <button>
+                <BackIcon class="w-8 " />
+            </button>
+            <p>Now Playing</p>
+            <button>
+                <BackIcon class="w-8 rotate-180" />
+            </button>
+        </div>
 
-            <!-- Image Banner -->
-            <img class="rounded-3xl w-4/6 aspect-square object-cover mx-auto mb-4"
-                src="https://source.unsplash.com/1600x900/?nature,water" alt="">
-            <h1 class="text-2xl text-center">Appalachian spring</h1>
-            <p class="text-center mb-8">Aaron Copland</p>
+        <audio ref="audio" :src="currentAudio.src" @timeupdate="upateTime"></audio>
 
-            <div class="flex justify-between px-4">
-                <p class="flex gap-1 items-center"> <span>
-                        <PlayIcon class="w-3" />
-                    </span>{{ durationPassed }}</p>
-                <p class="text-gray-400">{{ duration }}</p>
-            </div>
+        <!-- Image Banner -->
+        <div class="relative overflow-hidden w-4/6 aspect-[123/121] mx-auto rounded-3xl shadow-lg shadow-gray-900 mb-8">
+            <img class=" w-full h-full object-cover" src="https://source.unsplash.com/1600x900/?nature,water" alt="">
+            <div class="absolute inset-0" style="box-shadow: inset 0 0 70px 50px #000000ad;"></div>
+        </div>
+        <h1 class="text-2xl text-center">{{ currentAudio.name }}</h1>
+        <p class="text-center mb-12">{{ currentAudio.author }}</p>
 
-            <div>
-                <svg class="w-4/6 mx-auto rotate-[270deg]" viewBox="0 0 100 100">
-                    <defs>
-                        <filter id="f2" x="0" y="0" xmlns="http://www.w3.org/2000/svg">
-                            <feGaussianBlur in="SourceGraphic" stdDeviation="55" />
-                        </filter>
-                        <clipPath id="myCircle">
-                            <circle filter="url(#f2)" cx="50" cy="50" r="45" fill="#FFFFFF" />
-                        </clipPath>
-                    </defs>
-                    <image transform="rotate(90, 50 50)" width="200" height="120"
-                        xlink:href="https://source.unsplash.com/1600x900/?nature,water" clip-path="url(#myCircle)" />
+        <div class="flex justify-between px-4">
+            <p class="flex gap-1 items-center"> <span>
+                    <PlayIcon class="w-3" />
+                </span>{{ durationPassed }}</p>
+            <p class="text-[#927A58]">{{ displayTotalDuration }}</p>
+        </div>
 
-                    <circle cx="50" cy="50" r="45" fill="#463627de"></circle>
-                    <circle cx="50" cy="50" r="45" s troke-dashoffset="10"
-                        class="stroke-[#CBAD90] stroke-[2] transition-all" :style="{
-                'stroke-dasharray': 440, 'stroke-dashoffset': displayProgressSpinner, 'stroke-linecap': 'round'
-            }" fill="transparent"></circle>
-                    <circle :style="{
-                'rotate': displayProgressdot,
-                'transform-origin': '50%'
-            }" cx="95" cy="50" r="2" fill="#FEEED4"></circle>
-                    <circle @click="back" class="cursor-pointer" cx="50" cy="20" r="3" fill="#FEEED4"></circle>
-                    <circle @click="forward" class="cursor-pointer" cx="50" cy="80" r="3" fill="#FEEED4"></circle>
-                    <circle @click="volumnDown" class="cursor-pointer" cx="20" cy="50" r="3" fill="#FEEED4"></circle>
-                    <circle @click="volumnUp" class="cursor-pointer" cx="80" cy="50" r="3" fill="#FEEED4"></circle>
-                    <g @click="play" class="cursor-pointer">
-                        <circle cx="50" cy="50" r="20" fill="#624B33"></circle>
-                        <circle cx="50" cy="50" r="15" fill="#E4A06B"></circle>
-                        <polygon v-if="isPlaying" points="45,45 55,45 50,55" fill="#fff" />
+        <div>
+            <div class="relative aspect-square rounded-full overflow-hidden">
+                <img class="w-full h-full object-cover" src="https://source.unsplash.com/1600x900/?nature,water" alt="">
+                <div class="absolute inset-0 rounded-full"
+                    style="backdrop-filter: blur(15px);background-color: #6f59449e; inset: 0;">
+                </div>
 
-                        <line v-if="!isPlaying" x1="55" y1="48" x2="45" y2="48" stroke="#fff" stroke-width="2" />
-                        <line v-if="!isPlaying" x1="55" y1="53" x2="45" y2="53" stroke="#fff" stroke-width="2" />
+                <svg viewBox="0 0 1726 1726" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute inset-0">
+
+                    <circle cx="863" cy="863" r="425" fill="#997658" fill-opacity="0.78" shape-rendering="crispEdges" />
+                    <circle @click="back" cx="236" cy="863" r="37" fill="#FEECD6" />
+                    <circle @click="volumnUp" cx="863" cy="236" r="37" fill="#FEECD6" />
+                    <circle @click="forward" cx="1490" cy="863" r="37" fill="#FEECD6" />
+                    <circle @click="volumnDown" cx="863" cy="1489" r="37" fill="#FEECD6" />
+
+                    <g @click="play">
+                        <circle cx="863" cy="863" r="332" fill="url(#paint0_linear_0_1)" />
+                        <path d="M872 866.5L764.75 953.536L764.75 779.464L872 866.5Z" fill="#FEECD6" />
+                        <rect x="872" y="758" width="31" height="201" fill="#FEECD6" />
+                        <rect x="919" y="758" width="30" height="201" fill="#FEECD6" />
                     </g>
 
+                    <defs>
+                        <linearGradient id="paint0_linear_0_1" x1="1136" y1="863" x2="604" y2="927.5"
+                            gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FFCD88" />
+                            <stop offset="0.48" stop-color="#B47E52" />
+                            <stop offset="0.74621" stop-color="#90643D" />
+                            <stop offset="1" stop-color="#A37B4A" />
+                        </linearGradient>
+                    </defs>
+                    <circle cx="863" cy="863" r="840" stroke-dashoffset="10"
+                        class="stroke-[#CBAD90] stroke-[30] transition-all" :style="{
+            'stroke-dasharray': 5303, 'stroke-dashoffset': displayProgressSpinner, 'stroke-linecap': 'round'
+        }" />
                 </svg>
             </div>
-            <p class=" px-4">0002/0069</p>
+
         </div>
+        <p class=" px-4">0002/0069</p>
     </div>
 </template>
 
 <script setup lang="ts">
 import BackIcon from "@/components/Icons/BackIcon.vue";
 import PlayIcon from "@/components/Icons/PlayIcon.vue";
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, reactive } from "vue";
 
 const audio = ref<HTMLAudioElement | null>(null)
-const currentTime = ref(0)
-const duration = ref('00:00')
-const isPlaying = ref(false)
+const currentAudio = reactive({
+    src: 'music/HERO_One_Punch_Man_Epic_Orchestra.mp3',
+    name: "Appalachian spring",
+    author: "Aaron Copland",
+    currentDurationPausedAt: 0,
+    totalDuration: 0,
+    isPlaying: false,
+})
 
 function play() {
     if (!audio.value) return
     if (audio.value.duration > 0 && !audio.value.paused) {
         audio.value.pause()
-        isPlaying.value = false
+        currentAudio.isPlaying = false
     } else {
         audio.value.play()
-        isPlaying.value = true
+        currentAudio.isPlaying = true
     }
 }
 
 const displayProgressSpinner = computed(() => {
-    return 440 - ((getProgress(currentTime.value) / 100) * (440 - 158))
+    return 5303 - ((getProgress(currentAudio.currentDurationPausedAt) / 100) * (5303 - 23))
 })
 const displayProgressdot = computed(() => {
-    const value = ((getProgress(currentTime.value) / 100) * 360)
+    const value = ((getProgress(currentAudio.currentDurationPausedAt) / 100) * 360)
     return value + 'deg'
 })
 
 const durationPassed = computed(() => {
     if (!audio.value) return '00:00'
-    const minutes = Math.floor(currentTime.value / 60);
-    const seconds = Math.floor(currentTime.value % 60);
+    const minutes = Math.floor(currentAudio.currentDurationPausedAt / 60);
+    const seconds = Math.floor(currentAudio.currentDurationPausedAt % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+})
+const displayTotalDuration = computed(() => {
+    if (!audio.value) return '00:00'
+    const minutes = Math.floor(currentAudio.totalDuration / 60);
+    const seconds = Math.floor(currentAudio.totalDuration % 60);
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
     return `${minutes}:${returnedSeconds}`;
 })
 
 
-
 function upateTime() {
     if (!audio.value) return
-    currentTime.value = audio.value.currentTime;
+    currentAudio.currentDurationPausedAt = audio.value.currentTime;
 }
 
 function getProgress(currentTime: number) {
@@ -134,12 +150,12 @@ function volumnDown() {
 function back() {
     if (!audio.value) return
     audio.value.currentTime = Math.max(audio.value.currentTime - 10, 0)
-    currentTime.value = audio.value.currentTime
+    currentAudio.currentDurationPausedAt = audio.value.currentTime
 }
 function forward() {
     if (!audio.value) return
     audio.value.currentTime = Math.min(audio.value.currentTime + 10, audio.value.duration)
-    currentTime.value = audio.value.currentTime
+    currentAudio.currentDurationPausedAt = audio.value.currentTime
 }
 
 
@@ -148,10 +164,7 @@ function forward() {
 onMounted(() => {
     setTimeout(() => {
         if (!audio.value) return
-        const minutes = Math.floor(audio.value.duration / 60);
-        const seconds = Math.floor(audio.value.duration % 60);
-        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-        duration.value = `${minutes}:${returnedSeconds}`;
+        currentAudio.totalDuration = audio.value.duration
     }, 500);
 })
 
